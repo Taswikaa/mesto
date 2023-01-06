bfccfb6b70e067217ea14f6ba386f89353936f3f
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-01-06 19:11:05 +0300

    basic functions

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__button_purpose_close')
+const popupCloseButton = document.querySelector('.popup__button_purpose_close');
+const popupSubmitButton = document.querySelector('.popup__button_purpose_submit');
+const popupForm = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-job');
+const pageName = document.querySelector('.profile__name');
+const pageJob = document.querySelector('.profile__job');
 
+const handleFormSubmit = function(e) {
+  e.preventDefault();
+
+  let formName = nameInput.value;
+  let formJob = jobInput.value;
+  
+  pageName.textContent = `${formName}`;
+  pageJob.textContent = `${formJob}`;
+
+  popup.classList.remove('popup_opened');
+}
 
 popupOpenButton.addEventListener('click', function() {
   popup.classList.add('popup_opened');
@@ -10,3 +27,5 @@ popupOpenButton.addEventListener('click', function() {
 popupCloseButton.addEventListener('click', function() {
   popup.classList.remove('popup_opened');
 })
+
+popupSubmitButton.addEventListener('click', handleFormSubmit);
