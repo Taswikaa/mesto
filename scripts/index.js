e29853b6e084f07f5f3d9c009618d9096842c84a
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-01-26 17:52:23 +0300

    now basic images loaded by JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -32,3 +32,43 @@ popupEditButton.addEventListener('click', function() {
 popupCloseButton.addEventListener('click', closePopup);
 
 popupForm.addEventListener('submit', handleFormSubmit);
+
+// Elements
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(el => {
+  const elementsTemplate = document.querySelector('#elements-template');
+  const elementsList = document.querySelector('.elements__wrapper');
+
+  const elementsItem = elementsTemplate.content.cloneNode(true);
+  elementsItem.querySelector('.elements__image').src = `${el.link}`;
+  elementsItem.querySelector('.elements__name').textContent = `${el.name}`;
+
+  elementsList.append(elementsItem);
+})
